c2ff92d7f90ba3bc01cb71c450f898b6b711b57b
= <=>
2022-06-27 16:14:41 -0300

    Rotas e Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $users =[
+            "nomes" => [
+             "José Lira",
+             "Jão Lira"
+            ]
+        ];
+        dd[$user];
+    }
 
+
+    public function show($id)
+    {
+        $idUser = $id;
+
+        dd("Id do usuário é ".$id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get("/users", [UserController::class, "index"])->name("users.index");
+Route::get("/users{id}", [UserController::class, "show"])->name("users.show");
